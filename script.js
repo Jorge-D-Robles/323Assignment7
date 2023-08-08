70261eadd9abce1cab1c22c4f33352281c937da7
Jorge Robles <robles8925@gmail.com>
2023-08-08 16:29:34 -0400

    assignment 7 done

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("browser-info").innerHTML =
+    "<strong>Navigator:</strong><br>" +
+    "appName: " + navigator.appName + "<br>" +
+    "product: " + navigator.product + "<br>" +
+    "appVersion: " + navigator.appVersion + "<br>" +
+    "userAgent: " + navigator.userAgent + "<br>" +
+    "platform: " + navigator.platform + "<br>" +
+    "language: " + navigator.language + "<br>" +
+    "<strong>Window:</strong><br>" +
+    "innerHeight: " + window.innerHeight + "<br>" +
+    "innerWidth: " + window.innerWidth + "<br>" +
+    "<strong>Screen:</strong><br>" +
+    "width: " + screen.width + "<br>" +
+    "height: " + screen.height + "<br>" +
+    "availWidth: " + screen.availWidth + "<br>" +
+    "availHeight: " + screen.availHeight + "<br>" +
+    "colorDepth: " + screen.colorDepth + "<br>" +
+    "pixelDepth: " + screen.pixelDepth + "<br>" +
+    "<strong>Location:</strong><br>" +
+    "href: " + window.location.href + "<br>" +
+    "hostname: " + window.location.hostname + "<br>" +
+    "pathname: " + window.location.pathname + "<br>" +
+    "protocol: " + window.location.protocol;
